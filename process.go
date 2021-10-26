0c09cf817b3fcfe3a5c00321a797239b7b4e4d4e
Jaya Ojha <59759265+jaya271999@users.noreply.github.com>
2021-10-26 16:49:59 +0530

    Update process.go

--- process.go
+++ process.go
@@ -11,7 +11,7 @@ func main() {
     for _, file := range matches {
         target, _ := os.Readlink(file)
         if len(target) > 0 {
-            fmt.Printf("%+v\n", target)
+            fmt.Printf("%+v\n", filepath.Base(target))
         }
     }
 }
