e74c197998f83710e63d6f84f9b24a01471b0ecb
Jaya Ojha <59759265+jaya271999@users.noreply.github.com>
2021-10-27 12:34:11 +0530

    Create process2.go

---
+++ process2.go
@@ -0,0 +1,21 @@
+import (
+      ps "github.com/mitchellh/go-ps"
+      ... // other imports here...
+)
+
+func whatever(){
+    processList, err := ps.Processes()
+    if err != nil {
+        log.Println("ps.Processes() Failed, are you using windows?")
+        return
+    }
+
+    // map ages
+    for x := range processList {
+        var process ps.Process
+        process = processList[x]
+        log.Printf("%d\t%s\n",process.Pid(),process.Executable())
+
+        // do os.* stuff on the pid
+    }
+}
