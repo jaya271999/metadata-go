4e4d6cedc1e6266eb878a761e5004014991740ac
Jaya Ojha <59759265+jaya271999@users.noreply.github.com>
2021-11-08 16:42:40 +0530

    Update test.go

--- test.go
+++ test.go
@@ -1,80 +1,33 @@
-package main
+package ps
 
- import (
- 	"errors"
- 	"fmt"
- 	ps "github.com/mitchellh/go-ps"
- 	"os"
- 	"runtime"
- 	"strconv"
- 	"syscall"
- )
+// Process is the generic interface that is implemented on every platform
+// and provides common operations for processes.
+type Process interface {
+	// Pid is the process ID for this process.
+	Pid() int
 
- // check if the process is actually running
- // However, on Unix systems, os.FindProcess always succeeds and returns
- // a Process for the given pid...regardless of whether the process exists
- // or not.
- func getProcessRunningStatus(pid int) (*os.Process, error) {
- 	proc, err := os.FindProcess(pid)
- 	if err != nil {
- 		return nil, err
- 	}
+	// PPid is the parent process ID for this process.
+	PPid() int
 
- 	//double check if process is running and alive
- 	//by sending a signal 0
- 	//NOTE : syscall.Signal is not available in Windows
+	// Executable name running this process. This is not a path to the
+	// executable.
+	Executable() string
+}
 
- 	err = proc.Signal(syscall.Signal(0))
- 	if err == nil {
- 		return proc, nil
- 	}
+// Processes returns all processes.
+//
+// This of course will be a point-in-time snapshot of when this method was
+// called. Some operating systems don't provide snapshot capability of the
+// process table, in which case the process table returned might contain
+// ephemeral entities that happened to be running when this was called.
+func Processes() ([]Process, error) {
+	return processes()
+}
 
- 	if err == syscall.ESRCH {
- 		return nil, errors.New("process not running")
- 	}
-
- 	// default
- 	return nil, errors.New("process running but query operation not permitted")
- }
-
- func main() {
-
- 	if len(os.Args) != 2 {
- 		fmt.Printf("Usage : %s processID \n ", os.Args[0]) // return the program name back to %s
- 		os.Exit(1)                                         // graceful exit
- 	}
-
- 	pid, err := strconv.Atoi(os.Args[1])
-
- 	if err != nil {
- 		fmt.Println("Bad process ID supplied")
- 		os.Exit(-1)
- 	}
-
- 	//NOTE : syscall.Signal is not available in Windows
- 	if runtime.GOOS != "windows" {
- 		_, err := getProcessRunningStatus(int(pid))
-
- 		if err != nil {
- 			fmt.Println("Error : ", err)
- 			os.Exit(-1)
- 		}
-
- 	}
-
- 	// at this stage the Processes related functions found in Golang's OS package
- 	// is no longer sufficient, we will use Mitchell Hashimoto's https://github.com/mitchellh/go-ps
- 	// package to find the application/executable/binary name behind the process ID.
-
- 	p, err := ps.FindProcess(pid)
-
- 	if err != nil {
- 		fmt.Println("Error : ", err)
- 		os.Exit(-1)
- 	}
-
- 	fmt.Println("Process ID : ", p.Pid())
- 	fmt.Println("Parent Process ID : ", p.PPid())
- 	fmt.Println("Process ID binary name : ", p.Executable())
-
- }
+// FindProcess looks up a single process by pid.
+//
+// Process will be nil and error will be nil if a matching process is
+// not found.
+func FindProcess(pid int) (Process, error) {
+	return findProcess(pid)
+}
