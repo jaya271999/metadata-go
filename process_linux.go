1beb7b86af49ff804781f04800b79e9d6997a71f
Jaya Ojha <59759265+jaya271999@users.noreply.github.com>
2021-11-08 12:08:20 +0530

    Update process_linux.go

--- process_linux.go
+++ process_linux.go
@@ -1,33 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"strings"
+    "fmt"
+    "os"
+    "path/filepath"
 )
 
-// Refresh reloads all the data associated with this process.
-func (p *UnixProcess) Refresh() error {
-	statPath := fmt.Sprintf("/proc/%d/stat", p.pid)
-	dataBytes, err := ioutil.ReadFile(statPath)
-	if err != nil {
-		return err
-	}
-
-	// First, parse out the image name
-	data := string(dataBytes)
-	binStart := strings.IndexRune(data, '(') + 1
-	binEnd := strings.IndexRune(data[binStart:], ')')
-	p.binary = data[binStart : binStart+binEnd]
-
-	// Move past the image name and start parsing the rest
-	data = data[binStart+binEnd+2:]
-	_, err = fmt.Sscanf(data,
-		"%c %d %d %d",
-		&p.state,
-		&p.ppid,
-		&p.pgrp,
-		&p.sid)
-
-	return err
+func main() {
+    matches, err := filepath.Glob("/proc/*/exe")
+    for _, file := range matches {
+        target, _ := os.Readlink(file)
+        if len(target) > 0 {
+            fmt.Printf("%+v\n", target)
+        }
+    }
 }
