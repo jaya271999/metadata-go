e6146782cff4597206e5c8b4910c2e77e6121c02
Jaya Ojha <59759265+jaya271999@users.noreply.github.com>
2021-11-08 12:15:24 +0530

    Update process_linux.go

--- process_linux.go
+++ process_linux.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-    /*matches,*/ err := filepath.Glob("/proc/*/exe")
+    matches := filepath.Glob("/proc/*/exe")
     for _, file := range matches {
         target, _ := os.Readlink(file)
         if len(target) > 0 {
